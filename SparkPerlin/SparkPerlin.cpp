4d342a0b62ccd8066513d14cbd24cbb7354aa88e
Seamus <seamustparkinson@gmail.com>
2026-04-01 20:46:39 +0100

    This is a bracket surrounding ImGUI, the reason I have added this is because I want to be able to view and modify the perlin image in real time before choosing where I want to save it or not. There is no real implementation for the ImGui but I have put in all the base code, it needs cleaning up and improving.

--- SparkPerlin/SparkPerlin.cpp
+++ SparkPerlin/SparkPerlin.cpp
@@ -1,33 +1,71 @@
-// SparkPerlin.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "PerlinBase/PerlinNoise.hpp"
-#include "PerlinImage.h"
+#include "PerlinBase/PerlinImage.h"
+#include <Windows.h>
+#include <d3d11.h>
+#include <tchar.h>
+#include <dxgi.h>
+
+#include "ImGUI/imgui.h"
+#include "ImGUI/imgui_impl_dx11.h"
+#include "ImGUI/imgui_impl_win32.h"
+
+#pragma comment (lib,"d3d11.lib")
+#pragma comment (lib,"dxgi.lib")
+
+// Data
+static ID3D11Device* g_pd3dDevice = nullptr;
+static ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain* g_pSwapChain = nullptr;
+static bool                     g_SwapChainOccluded = false;
+static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
+static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+
+// Forward declarations of helper functions
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void CreateRenderTarget();
+void CleanupRenderTarget();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 
 int main()
 {
-	siv::PerlinNoise perlinA{ std::random_device{} };
+	double frequency;
+	std::cout << "double frequency = ";
+	std::cin >> frequency;
+	frequency = std::clamp(frequency, 0.1, 64.0);
 
-	siv::BasicPerlinNoise<double> perlinB;
+	std::int32_t octaves;
+	std::cout << "int32 octaves    = ";
+	std::cin >> octaves;
+	octaves = std::clamp(octaves, 1, 16);
 
-	for (std::int32_t y = 0; y < 10; ++y)
-	{
-		for (std::int32_t x = 0; x < 10; ++x)
-		{
-			const double noise = perlinA.octave2D_01(x * 0.1, y * 0.1, 6, 0.45);
-			std::cout << static_cast<int>(std::floor(noise * 10) - 0.5) << "\t";
-		}
-		std::cout << '\n';
-	}
+	std::uint32_t seed;
+	std::cout << "Enter '0' for a random uint32 seed      = ";
+	std::cin >> seed;
+
+	double persistance;
+	std::cout << "0-100: The smaller the number the higher the correlation between values\nEnter Persistance      = ";
+	std::cin >> persistance;
+	persistance = std::lerp(0, 1, (persistance/100));
+
+
+	if (seed == 0) { seed = std::random_device{}(); }
+
+	siv::PerlinNoise perlinA{ seed };
+
+//	for (std::int32_t y = 0; y < 10; ++y)
+//	{
+//		for (std::int32_t x = 0; x < 10; ++x)
+//		{
+//			const double noise = perlinA.octave2D_01(x * 0.1, y * 0.1, octaves, persistance);
+//			std::cout << static_cast<int>(std::floor(noise * 10) - 0.5) << "\t";
+//		}
+//		std::cout << '\n';
+//	}
 
 	Image image{ 512,512 };
-
-	double frequency = std::clamp(0.5, 0.1, 64.0);
-
-	std::int32_t octaves = std::clamp(24, 1, 16);
-
 
 	const double xFrequency = (frequency / image.width());
 	const double yFrequency = (frequency / image.height());
@@ -42,12 +80,13 @@ int main()
 	}
 
 	std::stringstream ss;
-	ss << 'f' << frequency << 'o' << octaves << ".bmp";
+	ss << 'f' << frequency << 'o' << octaves << '_' << seed << ".bmp";
 
 	if (image.saveBMP(ss.str()))
 	{
 		std::cout << "...saved \"" << ss.str() << "\"\n";
 	}
+
 
 	//for (std::int32_t x = 0; x < 10; ++x)
 	//{
@@ -55,6 +94,93 @@ int main()
 	//	std::cout << static_cast<int>(std::floor(noise * 10) - 0.5) << "\t";
 	//}
 	//std::cout << '\n';
-
 }
 
+// Helper functions
+
+bool CreateDeviceD3D(HWND hWnd)
+{
+    // Setup swap chain
+    // This is a basic setup. Optimally could use e.g. DXGI_SWAP_EFFECT_FLIP_DISCARD and handle fullscreen mode differently. See #8979 for suggestions.
+    DXGI_SWAP_CHAIN_DESC sd;
+    ZeroMemory(&sd, sizeof(sd));
+    sd.BufferCount = 2;
+    sd.BufferDesc.Width = 0;
+    sd.BufferDesc.Height = 0;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferDesc.RefreshRate.Numerator = 60;
+    sd.BufferDesc.RefreshRate.Denominator = 1;
+    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.SampleDesc.Quality = 0;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    UINT createDeviceFlags = 0;
+    //createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+    D3D_FEATURE_LEVEL featureLevel;
+    const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+    HRESULT res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res == DXGI_ERROR_UNSUPPORTED) // Try high-performance WARP software driver if hardware is not available.
+        res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_WARP, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
+    if (res != S_OK)
+        return false;
+
+    CreateRenderTarget();
+    return true;
+}
+
+void CleanupDeviceD3D()
+{
+    CleanupRenderTarget();
+    if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = nullptr; }
+    if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = nullptr; }
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+}
+
+void CreateRenderTarget()
+{
+    ID3D11Texture2D* pBackBuffer;
+    g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&pBackBuffer));
+    g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+    pBackBuffer->Release();
+}
+
+void CleanupRenderTarget()
+{
+    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
+}
+
+// Forward declare message handler from imgui_impl_win32.cpp
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+// Win32 message handler
+// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+            return 0;
+        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeHeight = (UINT)HIWORD(lParam);
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+}
